399a3c0fe1c50d28f1613ae65b22b7530b1ef8c7
Ronald/ <picardalronald3@gmail.com>
2025-09-28 21:27:53 +0800

    Create day12.py

---
+++ day12.py
@@ -0,0 +1,12 @@
+#loop through a numeric array preferrably not using manual index increment
+names  = [
+ 
+ 'Ronald',    
+ 'jenny',
+ 'picardal',
+ 'roque',
+     
+ ]
+
+for name in names:
+    print(name)
