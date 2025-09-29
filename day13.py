2fe5030fd13c7b7f47d28152fd383ae027007c86
Ronald/ <picardalronald3@gmail.com>
2025-09-29 15:13:09 +0800

    Create day13.py

---
+++ day13.py
@@ -0,0 +1,13 @@
+#loop through a numeric array printing both the index and the value
+
+names  = [
+ 
+ 'Ronald',    
+ 'jenny',
+ 'picardal',
+ 'roque',
+     
+ ]
+
+for index, value  in enumerate(names):
+    print(f"{index} : {value}")
