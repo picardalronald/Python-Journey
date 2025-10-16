7bd6a42086c4c1257a3671620caedbf92f1c9709
Ronald/ <picardalronald3@gmail.com>
2025-10-16 13:57:22 +0800

    Create day25.py

---
+++ day25.py
@@ -0,0 +1,15 @@
+# Create function: add(x,y) that returns the sum of 2 numbers
+
+def add(x,y):
+    sum = x + y
+    return sum
+
+
+x = int(input("Input first number: "))
+y = int(input("Input second number: "))
+
+print(f"Total sum of two numbers: {add(x,y)}")
+
+
+  
+    
