6f53c2f542af1ca86ac151193b87ca33712648be
Ronald/ <picardalronald3@gmail.com>
2025-10-19 12:18:20 +0800

    Create day28.py

---
+++ day28.py
@@ -0,0 +1,9 @@
+# Create a function square(x) that returns the square of the number
+
+def square(x):
+   return x * x
+
+
+x = int(input("Enter the number: "))
+
+print(f"Square of {x} = {square(x)}")
