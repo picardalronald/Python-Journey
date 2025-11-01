2632ad0e9f5f632909d481044b02b1f9ef6f7d45
Rondiv/ <picardalronald3@gmail.com>
2025-11-01 13:58:33 +0800

    Create day35.py

---
+++ day35.py
@@ -0,0 +1,7 @@
+# ask for a year input and output if it's a leap year or not using built in functions if possible
+year = int(input("Enter a year: "))
+
+if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
+    print(f"it is a leap year {year}")
+else:
+    print(f"it is not leap year {year}")
