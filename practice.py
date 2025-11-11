98ae195e63206be8d6b97a40638177449ae406f9
picardalronald <picardalronald3@gmail.com>
2025-11-11 20:08:38 +0800

    Save all local changes before rebase

--- practice.py
+++ practice.py
@@ -1,56 +1,10 @@
-# Practice OOP
+senior = 60
 
-# child class with an object and properties
-class Personal:
-    # initialization for those objects
-    def __init__(self, name, age, gender):
-        self.name = name
-        self.age = age
-        self.gender = gender
-        
-    def greetings(self):
-        print(f"What your name? : {self.name}")
-        print(f"How old are you? : {self.age}")
-        print(f"What your gender? : {self.gender}")
-        
+print("You get 20% discount for a senior 60+ age")
+age = int(input("Enter Your Age: "))
 
-
-# inherit in parent class
-class Ronald:
-    def __init__(self, name, lastname, age, gender, ):
-     self.name = name
-    #  Private oject using encapsulation
-     self.__lastname = lastname
-     self.age = age
-     self.gender = gender
-     
-    def greetings(self):
-        # private object can access
-        print(f"Hello sir {self.name + " " + self.__lastname}")
-        print(f"Your are {self.age} Yrs old right")
-        print(f"Are your {self.gender}")
+if age > senior:
+    print(f"You get the senior Discount")
     
-# pass the value of object here         
-ron = Ronald("Ronald", "Picardal", 21 , "Male")     
-
-# user input
-# names = input("Enter your name: ")
-# ages = input("Enter your age: ")
-# genders = input("Enter your gender: ")
-
-# this is an object
-# result = Personal(names, ages, genders)
-
-# print result here 
-
-# print("\nHere the list of Question:")
-# result.greetings()
-
-# print result here 
-print("\nHere the list of your Information:")
-ron.greetings()
-
-
-        
-        
-        
+else:
+   print("Your age is not eliagable on senior discount")
